9c3491f11df6e96800ac2e7ce05dc94924608c0a
SamuelKraut <krauts.tin21@student.dhbw-heidenheim.de>
2024-03-18 09:31:48 +0100

    fix main.ts check problem

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import "./app.css";
 import App from "./App.svelte";
 
+const targetElement = document.getElementById("app");
+if (!targetElement) throw new Error("Cannot find element with ID 'app'");
+
 const app = new App({
-  target: document.getElementById("app"),
+  target: targetElement,
 });
 
 export default app;
